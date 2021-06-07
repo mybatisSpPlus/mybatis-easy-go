ee427199d0f7c129cb1ac8d814c8a422d66e03b3
zhouyu <zhouyu74748585@gmail.com>
2021-06-07 18:05:05 +0800

    增加Nolimit，便于sql构建
    Signed-off-by: zhouyu <zhouyu74748585@gmail.com>

---
+++ mybatis-sp-plus-core/src/main/java/com/github/mybatis/sp/plus/actions/NoLimit.java
@@ -0,0 +1,13 @@
+package com.github.mybatis.sp.plus.actions;
+
+import com.github.mybatis.sp.plus.exception.SelfCheckException;
+
+/**
+ * @author zhouyu74748585@hotmail.com
+ * @date 2021/4/8 11:00
+ */
+public class NoLimit extends Limit {
+    @Override
+    public void selfCheck() throws SelfCheckException {
+    }
+}
