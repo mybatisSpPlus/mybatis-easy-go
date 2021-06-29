68997ddb5ea79cdd563f565c6a5dd7c7a1f19cdb
zhouyu <zhouyu74748585@gmail.com>
2021-06-29 16:55:14 +0800

    增加注释
    Signed-off-by: zhouyu <zhouyu74748585@gmail.com>

--- mybatis-easy-go-core/src/main/java/com/github/mybatis/easy/go/ConditionMethods.java
+++ mybatis-easy-go-core/src/main/java/com/github/mybatis/easy/go/ConditionMethods.java
@@ -13,148 +13,384 @@ import com.github.mybatis.easy.go.meta.Table;
  */
 public class ConditionMethods {
 
+    /**
+     * 返回一个空的条件，用于占位，在生成语句时会被去掉
+     *
+     * @return
+     */
     public static EmptyCondition emptyCondition() {
         return new EmptyCondition();
     }
 
+    /**
+     * 返回一个 与 条件
+     *
+     * @param conditions 传入的条件，所有条件需要同时满足
+     * @return
+     */
     public static And and(Condition... conditions) {
         return new And(conditions);
     }
 
+    /**
+     * 返回一个 或 条件
+     *
+     * @param conditions 传入的条件，所有条件至少需要满足一个
+     * @return
+     */
     public static Or or(Condition... conditions) {
         return new Or(conditions);
     }
 
-    public static Between between(Field field, Object startValue, Object endValue) {
-        return new Between(field, startValue, endValue);
-    }
-
-    public static Eq eq(Field field, Object value) {
-        return new Eq(field, value);
-    }
-
-    public static Neq neq(Field field, Object value) {
-        return new Neq(field, value);
-    }
-
-    public static Gt gt(Field field, Object value) {
-        return new Gt(field, value);
-    }
-
-    public static Gte gte(Field field, Object value) {
-        return new Gte(field, value);
-    }
-
-    public static Lt lt(Field field, Object value) {
-        return new Lt(field, value);
-    }
-
-    public static Lte lte(Field field, Object value) {
-        return new Lte(field, value);
-    }
-
-    public static Like like(Field field, String value) {
-        return new Like(field, value);
-    }
-
-    public static StartWith startWith(Field field, String value) {
-        return new StartWith(field, value);
-    }
-
-    public static EndWith endWith(Field field, String value) {
-        return new EndWith(field, value);
-    }
-
-    public static Regx regx(Field field, String value) {
-        return new Regx(field, value);
-    }
-
-    public static In in(Field field, Object... values) {
-        return new In(field, values);
-    }
-
-    public static In in(Field field, Table table) {
-        return new In(field, table);
-    }
-
-    public static IsNull isNull(Field field) {
-        return new IsNull(field);
-    }
-
-    public static IsNotNull isNotNull(Field field) {
-        return new IsNotNull(field);
-    }
-
+    /**
+     * 返回一个 否 条件
+     *
+     * @param condition 传入的条件
+     * @return
+     */
     public static Not not(Condition condition) {
         return new Not(condition);
     }
 
+    /**
+     * 返回一个 范围限定 条件，
+     *
+     * @param field      字段
+     * @param startValue 最小值
+     * @param endValue   最大值
+     * @return
+     */
+    public static Between between(Field field, Object startValue, Object endValue) {
+        return new Between(field, startValue, endValue);
+    }
+
+    /**
+     * 返回一个 范围限定 条件，
+     *
+     * @param fieldName  字段名
+     * @param startValue 最小值
+     * @param endValue   最大值
+     * @return
+     */
     public static Between between(String fieldName, Object startValue, Object endValue) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Between(field, startValue, endValue);
     }
 
+    /**
+     * 返回一个 等于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Eq eq(Field field, Object value) {
+        return new Eq(field, value);
+    }
+
+    /**
+     * 返回一个 等于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Eq eq(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Eq(field, value);
     }
 
+    /**
+     * 返回一个 不等于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Neq neq(Field field, Object value) {
+        return new Neq(field, value);
+    }
+
+    /**
+     * 返回一个 不等于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Neq neq(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Neq(field, value);
     }
 
+    /**
+     * 返回一个 大于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Gt gt(Field field, Object value) {
+        return new Gt(field, value);
+    }
+
+    /**
+     * 返回一个 大于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Gt gt(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Gt(field, value);
     }
 
+    /**
+     * 返回一个 大于等于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Gte gte(Field field, Object value) {
+        return new Gte(field, value);
+    }
+
+    /**
+     * 返回一个 大于等于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Gte gte(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Gte(field, value);
     }
 
+    /**
+     * 返回一个 小于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Lt lt(Field field, Object value) {
+        return new Lt(field, value);
+    }
+
+    /**
+     * 返回一个 小于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Lt lt(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Lt(field, value);
     }
 
+    /**
+     * 返回一个 小于等于 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Lte lte(Field field, Object value) {
+        return new Lte(field, value);
+    }
+
+    /**
+     * 返回一个 小于等于 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Lte lte(String fieldName, Object value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Lte(field, value);
     }
 
+    /**
+     * 返回一个 模糊匹配 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static Like like(Field field, String value) {
+        return new Like(field, value);
+    }
+
+    /**
+     * 返回一个 模糊匹配 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static Like like(String fieldName, String value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Like(field, value);
     }
 
+    /**
+     * 返回一个 以...开始 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static StartWith startWith(Field field, String value) {
+        return new StartWith(field, value);
+    }
+
+    /**
+     * 返回一个 以...开始 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static StartWith startWith(String fieldName, String value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new StartWith(field, value);
     }
 
+    /**
+     * 返回一个 以...结尾 条件
+     *
+     * @param field 字段
+     * @param value 判断值
+     * @return
+     */
+    public static EndWith endWith(Field field, String value) {
+        return new EndWith(field, value);
+    }
+
+
+    /**
+     * 返回一个 以...结尾 条件
+     *
+     * @param fieldName 字段名
+     * @param value     判断值
+     * @return
+     */
     public static EndWith endWith(String fieldName, String value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new EndWith(field, value);
     }
 
+    /**
+     * 返回一个 正则匹配 条件
+     *
+     * @param field 字段
+     * @param value 正则表达式
+     * @return
+     */
+    public static Regx regx(Field field, String value) {
+        return new Regx(field, value);
+    }
 
+    /**
+     * 返回一个 正则匹配 条件
+     *
+     * @param fieldName 字段名
+     * @param value     正则表达式
+     * @return
+     */
     public static Regx regx(String fieldName, String value) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new Regx(field, value);
     }
 
+    /**
+     * 返回一个 在...其中 条件
+     *
+     * @param field  字段
+     * @param values 值的范围
+     * @return
+     */
+    public static In in(Field field, Object... values) {
+        return new In(field, values);
+    }
+
+    /**
+     * 返回一个 在...其中 条件
+     *
+     * @param fieldName 字段名
+     * @param values    值的范围
+     * @return
+     */
     public static In in(String fieldName, Object... values) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new In(field, values);
     }
 
+    /**
+     * 返回一个 在...其中 条件
+     *
+     * @param field 字段
+     * @param table 子查询对象
+     * @return
+     */
+    public static In in(Field field, Table table) {
+        return new In(field, table);
+    }
+
+    /**
+     * 返回一个 在...其中 条件
+     *
+     * @param fieldName 字段名
+     * @param table     子查询对象
+     * @return
+     */
+    public static In in(String fieldName, Table table) {
+        Field field = QueryBuilderHelper.fieldNameToField(fieldName);
+        return new In(field, table);
+    }
+
+    /**
+     * 返回一个 为空 条件
+     *
+     * @param field 字段
+     * @return
+     */
+    public static IsNull isNull(Field field) {
+        return new IsNull(field);
+    }
+
+    /**
+     * 返回一个 为空 条件
+     *
+     * @param fieldName 字段
+     * @return
+     */
     public static IsNull isNull(String fieldName) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new IsNull(field);
     }
 
+    /**
+     * 返回一个 不为空 条件
+     *
+     * @param field 字段
+     * @return
+     */
+    public static IsNotNull isNotNull(Field field) {
+        return new IsNotNull(field);
+    }
+
+    /**
+     * 返回一个 不为空 条件
+     *
+     * @param fieldName 字段
+     * @return
+     */
     public static IsNotNull isNotNull(String fieldName) {
         Field field = QueryBuilderHelper.fieldNameToField(fieldName);
         return new IsNotNull(field);
--- mybatis-easy-go-core/src/main/java/com/github/mybatis/easy/go/FunctionMethods.java
+++ mybatis-easy-go-core/src/main/java/com/github/mybatis/easy/go/FunctionMethods.java
@@ -9,74 +9,184 @@ import com.github.mybatis.easy.go.meta.Field;
  */
 public class FunctionMethods {
 
+    /**
+     * case when的开始
+     *
+     * @return
+     */
     public static Case caze() {
         return new Case();
     }
 
+    /**
+     * 计数方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Count count(Field field) {
         return new Count(field);
     }
 
-    public static Concat concat(Field... objs) {
-        return new Concat(objs);
+    /**
+     * 字段拼接方法
+     *
+     * @param fields 需要拼接的字段
+     * @return
+     */
+    public static Concat concat(Field... fields) {
+        return new Concat(fields);
     }
 
-    public static GroupConcat groupConcat(Field... objs) {
-        return new GroupConcat(objs);
+    /**
+     * 分组拼接字符串
+     *
+     * @param fields 需要拼接的字段
+     * @return
+     */
+    public static GroupConcat groupConcat(Field... fields) {
+        return new GroupConcat(fields);
     }
 
+    /**
+     * 格式化方法
+     *
+     * @param field  源字段
+     * @param format 字段名
+     * @return
+     */
     public static Format format(Field field, String format) {
         return new Format(field, format);
     }
 
+    /**
+     * 字符串截取的方法
+     *
+     * @param field 源字段
+     * @param start 开始位置
+     * @return
+     */
     public static Substr substr(Field field, Object start) {
         return new Substr(field, start);
     }
 
+    /**
+     * 字符串截取的方法
+     *
+     * @param field  源字段
+     * @param start  开始位置
+     * @param length 截取长度
+     * @return
+     */
     public static Substr substr(Field field, Object start, Object length) {
         return new Substr(field, start, length);
     }
 
+    /**
+     * 字符串编码转换的方法
+     *
+     * @param field         源字段
+     * @param targetCharset 目标编码
+     * @return
+     */
     public static Convert convert(Field field, String targetCharset) {
         return new Convert(field, targetCharset);
     }
 
+    /**
+     * 字符串编码转换的方法
+     *
+     * @param field         源字段
+     * @param targetCharset 目标编码
+     * @param sourceCharset 来源编码
+     * @return
+     */
     public static Convert convert(Field field, String targetCharset, String sourceCharset) {
         return new Convert(field, targetCharset, sourceCharset);
     }
 
+    /**
+     * 在字符串中查找目标字的方法
+     *
+     * @param field  源字段
+     * @param target 目标
+     * @return
+     */
     public static Instr instr(Field field, Object target) {
         return new Instr(field, target);
     }
 
+    /**
+     * 在字符串中查找目标字的方法
+     *
+     * @param field  源字段
+     * @param target 目标字符串
+     * @param start  起始位置
+     * @param times  匹配序号
+     * @return
+     */
     public static Instr instr(Field field, Object target, Object start, Object times) {
         return new Instr(field, target, start, times);
     }
 
+    /**
+     * 从右侧开始截取字符串的方法
+     *
+     * @param field  源字段
+     * @param length 截取长度
+     * @return
+     */
     public static Right right(Field field, int length) {
         return new Right(field, length);
     }
 
+    /**
+     * 从左侧开始截取字符串的方法
+     *
+     * @param field  源字段
+     * @param length 截取长度
+     * @return
+     */
     public static Left left(Field field, int length) {
         return new Left(field, length);
     }
 
+    /**
+     * 替换字符串的方法
+     *
+     * @param field  源字段
+     * @param oldStr 原字符串
+     * @param newStr 新字符串
+     * @return
+     */
     public static Replace replace(Field field, Object oldStr, Object newStr) {
         return new Replace(field, oldStr, newStr);
     }
 
+    /**
+     * 字符串转为小写的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Lcase lcase(Field field) {
         return new Lcase(field);
     }
 
+    /**
+     * 字段转为大写的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Ucase ucase(Field field) {
         return new Ucase(field);
     }
 
     /**
-     * 计算字符串长度
+     * 计算字符串长度的方法
      *
-     * @param field
+     * @param field 源字段
      * @return
      */
     public static Len len(Field field) {
@@ -84,63 +194,147 @@ public class FunctionMethods {
     }
 
     /**
-     * 计算字节长度长度
+     * 计算字节长度长度的方法
      *
-     * @param field
+     * @param field 源字段
      * @return
      */
     public static LenB lenB(Field field) {
         return new LenB(field);
     }
 
+    /**
+     * 计算字段平均值的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Avg avg(Field field) {
         return new Avg(field);
     }
 
+    /**
+     * 计算字段最大值的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Max max(Field field) {
         return new Max(field);
     }
 
+    /**
+     * 计算字段最小值的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Min min(Field field) {
         return new Min(field);
     }
 
+    /**
+     * 计算字段总值的方法
+     *
+     * @param field 源字段
+     * @return
+     */
     public static Sum sum(Field field) {
         return new Sum(field);
     }
 
+    /**
+     * 计算字段四舍五入值的方法
+     *
+     * @param field    源字段
+     * @param decimals 保留的小数位数
+     * @return
+     */
     public static Round round(Field field, int decimals) {
         return new Round(field, decimals);
     }
 
+    /**
+     * 获得当前系统时间的方法
+     *
+     * @return
+     */
     public static Now now() {
         return new Now();
     }
 
+    /**
+     * 加法
+     *
+     * @param a 加数a
+     * @param b 加数b
+     * @return
+     */
     public static Add add(Object a, Object b) {
         return new Add(a, b);
     }
 
+    /**
+     * 除法
+     *
+     * @param a 被除数
+     * @param b 除数
+     * @return
+     */
     public static Divide divide(Object a, Object b) {
         return new Divide(a, b);
     }
 
+    /**
+     * 乘法
+     *
+     * @param a 乘数a
+     * @param b 乘数b
+     * @return
+     */
     public static Multiply multiply(Object a, Object b) {
         return new Multiply(a, b);
     }
 
+    /**
+     * 减法
+     *
+     * @param a 减数a
+     * @param b 减数b
+     * @return
+     */
     public static Subtract subtract(Object a, Object b) {
         return new Subtract(a, b);
     }
 
+    /**
+     * 求余/取模
+     *
+     * @param a 被除数
+     * @param b 除数
+     * @return
+     */
     public static Surplus surplus(Object a, Object b) {
         return new Surplus(a, b);
     }
 
+    /**
+     * 调用自定义函数
+     *
+     * @param functionName 函数名称
+     * @return
+     */
     public static CustomFunction customFunction(String functionName) {
         return new CustomFunction(functionName);
     }
 
+    /**
+     * 调用自定义函数
+     *
+     * @param functionName 函数名称
+     * @param parameters   参数列表
+     * @return
+     */
     public static CustomFunction customFunction(String functionName, Object... parameters) {
         return new CustomFunction(functionName, parameters);
     }
